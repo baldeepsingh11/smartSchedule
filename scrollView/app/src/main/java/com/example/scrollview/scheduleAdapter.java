97297ca776045f0aefbaf0eaac026744af10562e
anfal <anfalansari1@gmail.com>
2020-07-14 18:49:35 +0530

    visibility error fixed

--- scrollView/app/src/main/java/com/example/scrollview/scheduleAdapter.java
+++ scrollView/app/src/main/java/com/example/scrollview/scheduleAdapter.java
@@ -3,6 +3,7 @@ package com.example.scrollview;
 import android.content.Context;
 import android.transition.AutoTransition;
 import android.transition.TransitionManager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -22,7 +23,7 @@ import java.util.List;
 
 public class scheduleAdapter extends RecyclerView.Adapter<scheduleAdapter.ViewHolder> {
 
-
+    private static final String TAG = "scheduleAdapter";
     private List<Schedule> list;
     private Context mContext;
 
@@ -47,6 +48,7 @@ public class scheduleAdapter extends RecyclerView.Adapter<scheduleAdapter.ViewHo
         holder.venue.setText(schedule.getVenue());
         holder.time.setText(schedule.getTime());
         holder.type.setText(schedule.getType());
+
             /*if (schedule.getType().equals("Tutorial")){
                 holder.type.setBackground(mContext.getDrawable(R.drawable.bg_lecture));}
             else if (schedule.getType().equals("Lecture")){
@@ -60,19 +62,27 @@ public class scheduleAdapter extends RecyclerView.Adapter<scheduleAdapter.ViewHo
         final ConstraintLayout constraintLayout= holder.expandableLayout;
         final CardView cardView = holder.cardView;
         final TextView subjectCode = holder.subjectCode;
+        final TextView profName = holder.profName;
+        final TextView type = holder.type;
 
         holder.arrowBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (constraintLayout.getVisibility() == View.GONE) {
                     TransitionManager.beginDelayedTransition(cardView, new AutoTransition());
+                    Log.d(TAG, "onClick: before "+constraintLayout.getVisibility());
                     subjectCode.setVisibility(View.VISIBLE);
                     constraintLayout.setVisibility(View.VISIBLE);
+                    type.setVisibility(View.VISIBLE);
+                    profName.setVisibility(View.VISIBLE);
+                    Log.d(TAG, "onClick: after"+ constraintLayout.getVisibility());
                     button.animate().setDuration(500).rotationXBy(180);
                 } else {
                     TransitionManager.beginDelayedTransition(cardView, new AutoTransition());
                     subjectCode.setVisibility(View.GONE);
                     constraintLayout.setVisibility(View.GONE);
+                    type.setVisibility(View.GONE);
+                    profName.setVisibility(View.GONE);
                     button.animate().setDuration(500).rotationXBy(-180);
 
                 }
